327468c177c3066efdbbdd300a5adb385d9c55e8
Wasin Waeosri <wasin.waeosri@refinitiv.com>
2021-04-19 16:14:36 +0700

    reorg folder structure success

---
+++ advanceExamples/Consumer/src/main/java/com/refinitiv/ema/consumer/Consumer.java
@@ -0,0 +1,163 @@
+///*|----------------------------------------------------------------------------------------------------
+// *|            This source code is provided under the Apache 2.0 license      	--
+// *|  and is provided AS IS with no warranty or guarantee of fit for purpose.  --
+// *|                See the project's LICENSE.md for details.                  					--
+// *|           Copyright (C) 2021 Refinitiv. All rights reserved.            		--
+///*|----------------------------------------------------------------------------------------------------
+
+// This source code is based on EMA Java Consumer ex200_MP_Streaming example
+
+package com.refinitiv.ema.consumer;
+
+import com.refinitiv.ema.access.Msg;
+import com.refinitiv.ema.access.AckMsg;
+import com.refinitiv.ema.access.GenericMsg;
+import com.refinitiv.ema.access.RefreshMsg;
+import com.refinitiv.ema.access.StatusMsg;
+import com.refinitiv.ema.access.UpdateMsg;
+import com.refinitiv.ema.access.Data;
+import com.refinitiv.ema.access.DataType;
+import com.refinitiv.ema.access.DataType.DataTypes;
+import com.refinitiv.ema.access.EmaFactory;
+import com.refinitiv.ema.access.FieldEntry;
+import com.refinitiv.ema.access.FieldList;
+import com.refinitiv.ema.access.OmmConsumer;
+import com.refinitiv.ema.access.OmmConsumerClient;
+import com.refinitiv.ema.access.OmmConsumerEvent;
+import com.refinitiv.ema.access.OmmException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class AppClient implements OmmConsumerClient
+{
+	private static final Logger logger = LoggerFactory.getLogger(AppClient.class);
+
+	public void onRefreshMsg(RefreshMsg refreshMsg, OmmConsumerEvent event)
+	{
+		logger.info("Consumer: Receives Market Price Refresh message");
+
+		logger.info("Item Name: " + (refreshMsg.hasName() ? refreshMsg.name() : "<not set>"));
+		logger.info("Service Name: " + (refreshMsg.hasServiceName() ? refreshMsg.serviceName() : "<not set>"));
+
+		logger.info("Item State: " + refreshMsg.state());
+		
+		if (DataType.DataTypes.FIELD_LIST == refreshMsg.payload().dataType())
+			decode(refreshMsg.payload().fieldList());
+
+		System.out.println();
+	}
+	
+	public void onUpdateMsg(UpdateMsg updateMsg, OmmConsumerEvent event) 
+	{
+		logger.info("Consumer: Receives Market Price Update message");
+
+		logger.info("Item Name: " + (updateMsg.hasName() ? updateMsg.name() : "<not set>"));
+		logger.info("Service Name: " + (updateMsg.hasServiceName() ? updateMsg.serviceName() : "<not set>"));
+		
+		if (DataType.DataTypes.FIELD_LIST == updateMsg.payload().dataType())
+			decode(updateMsg.payload().fieldList());
+		
+		System.out.println();
+	}
+
+	public void onStatusMsg(StatusMsg statusMsg, OmmConsumerEvent event) 
+	{
+		logger.info("Item Name: " + (statusMsg.hasName() ? statusMsg.name() : "<not set>"));
+		logger.info("Service Name: " + (statusMsg.hasServiceName() ? statusMsg.serviceName() : "<not set>"));
+
+		if (statusMsg.hasState())
+			logger.info("Item State: " +statusMsg.state());
+		
+		System.out.println();
+	}
+	
+	public void onGenericMsg(GenericMsg genericMsg, OmmConsumerEvent consumerEvent){}
+	public void onAckMsg(AckMsg ackMsg, OmmConsumerEvent consumerEvent){}
+	public void onAllMsg(Msg msg, OmmConsumerEvent consumerEvent){}
+
+	void decode(FieldList fieldList)
+	{
+
+		for (FieldEntry fieldEntry : fieldList)
+		{
+			System.out.print("Fid: " + fieldEntry.fieldId() + " Name = " + fieldEntry.name() + " DataType: " + DataType.asString(fieldEntry.load().dataType()) + " Value: ");
+
+			if (Data.DataCode.BLANK == fieldEntry.code())
+				System.out.println(" blank");
+			else
+				switch (fieldEntry.loadType())
+				{
+				case DataTypes.REAL :
+					System.out.println(fieldEntry.real().asDouble());
+					break;
+				case DataTypes.DATE :
+					System.out.println(fieldEntry.date().day() + " / " + fieldEntry.date().month() + " / " + fieldEntry.date().year());
+					break;
+				case DataTypes.TIME :
+					System.out.println(fieldEntry.time().hour() + ":" + fieldEntry.time().minute() + ":" + fieldEntry.time().second() + ":" + fieldEntry.time().millisecond());
+					break;
+				case DataTypes.DATETIME :
+					System.out.println(fieldEntry.dateTime().day() + " / " + fieldEntry.dateTime().month() + " / " +
+						fieldEntry.dateTime().year() + "." + fieldEntry.dateTime().hour() + ":" + 
+						fieldEntry.dateTime().minute() + ":" + fieldEntry.dateTime().second() + ":" + 
+						fieldEntry.dateTime().millisecond() + ":" + fieldEntry.dateTime().microsecond()+ ":" + 
+						fieldEntry.dateTime().nanosecond());
+					break;
+				case DataTypes.INT :
+					System.out.println(fieldEntry.intValue());
+					break;
+				case DataTypes.UINT :
+					System.out.println(fieldEntry.uintValue());
+					break;
+				case DataTypes.ASCII :
+					System.out.println(fieldEntry.ascii());
+					break;
+				case DataTypes.ENUM :
+					System.out.println(fieldEntry.hasEnumDisplay() ? fieldEntry.enumDisplay() : fieldEntry.enumValue());
+					break;
+				case DataTypes.RMTES :
+					System.out.println(fieldEntry.rmtes());
+					break;
+				case DataTypes.ERROR :
+					System.out.println("(" + fieldEntry.error().errorCodeAsString() + ")");
+					break;
+				default :
+					System.out.println();
+					break;
+				}
+		}
+	}
+}
+
+public class Consumer 
+{
+	private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+	public static void main(String[] args)
+	{
+		OmmConsumer consumer = null;
+		try
+		{
+			logger.info("Starting Consumer application");
+
+			AppClient appClient = new AppClient();
+			
+			//consumer  = EmaFactory.createOmmConsumer(EmaFactory.createOmmConsumerConfig().host("localhost:14002").username("user"));
+			consumer  = EmaFactory.createOmmConsumer(EmaFactory.createOmmConsumerConfig().consumerName("Consumer_1").username("user"));
+			consumer.registerClient(EmaFactory.createReqMsg().serviceName("DIRECT_FEED").name("EUR="), appClient);
+			
+			Thread.sleep(60000);			// API calls onRefreshMsg(), onUpdateMsg() and onStatusMsg()
+		}
+		catch (InterruptedException | OmmException excp)
+		{
+			System.out.println(excp.getMessage());
+		}
+		finally 
+		{
+			if (consumer != null) consumer.uninitialize();
+		}
+	}
+}
+
+
